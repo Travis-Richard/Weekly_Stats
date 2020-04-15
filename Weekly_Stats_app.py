0d886075947f6bf997168697c1ef1ea722ad788c
Travis-Richard <t.richard008@gmail.com>
2020-04-15 13:26:28 -0600

    Added comments to show functionality of code

--- Weekly_Stats_app.py
+++ Weekly_Stats_app.py
@@ -346,6 +346,8 @@ class Ui_Weekly_Stats_Application(object):
         self.dates = [sunday + timedelta(days=x) for x in range((saturday - sunday).days + 1)]
         # print(self.dates)
 
+        # Need to add 6 hours of time to our inputs as the CSV file takes into account the timezone, we will covert back later on
+
         sunday = sunday + timedelta(hours=6)
         saturday = saturday + timedelta(hours=6)
 
@@ -354,12 +356,16 @@ class Ui_Weekly_Stats_Application(object):
             .format(sunday.strftime('%Y-%m-%d'), sunday.strftime('%H'), sunday.strftime('%M'),
                     sunday.strftime('%S'), saturday.strftime('%Y-%m-%d'), saturday.strftime('%H'),
                     saturday.strftime('%M'), saturday.strftime('%S'))
-        print(self.url_csv_retrieval)
+        # print(self.url_csv_retrieval)
 
     def excludeTimes(self):
         exc_time_s = []
         exc_time_e = []
         day_of_week = ['sun', 'mon', 'tues', 'wed', 'thur', 'fri', 'sat']
+
+
+        # This loop creates two lists of a start and end time for the exclusion times from the self.dates list. There will be 3 start and end times per day, for a total
+        # of 21 items in each list. Start times will be 00, 08, and 16 for each day, End times will be 07:59:59, 15:59:59 and 23:59:59
 
         for i in range(0, len(day_of_week)):
             for v in range(3):
@@ -375,7 +381,13 @@ class Ui_Weekly_Stats_Application(object):
                     exc_time_s.append(self.dates[i] + timedelta(hours=16))
                     exc_time_e.append(self.dates[i] + timedelta(hours=23, minutes=59, seconds=59))
 
+
+        # exec_time is a list of two lists, start and end exclusion times, this way this new list will have a format of [[mon_1st_shift_start, mon_1st_shift_end], ...]
+        # exc_time will have a length of 21, with 2 elements per item.
+
         exc_time = list(map(list, zip(exc_time_s, exc_time_e)))
+
+        # this list is a boolean list of true or false, depending if the button is selected or not. True if it is
 
         bool_list_btns = [self.sun_1st_btn.isChecked(), self.sun_2nd_btn.isChecked(), self.sun_3rd_btn.isChecked(),
                           self.mon_1st_btn.isChecked(), self.mon_2nd_btn.isChecked(), self.mon_3rd_btn.isChecked(),
@@ -384,18 +396,42 @@ class Ui_Weekly_Stats_Application(object):
                           self.thur_1st_btn.isChecked(), self.thur_2nd_btn.isChecked(), self.thur_3rd_btn.isChecked(),
                           self.fri_1st_btn.isChecked(), self.fri_2nd_btn.isChecked(), self.fri_3rd_btn.isChecked(),
                           self.sat_1st_btn.isChecked(), self.sat_2nd_btn.isChecked(), self.sat_3rd_btn.isChecked()]
+
+        # btn_exc_time creates a list of lists, in which the of lists is an item and a list of lists, or a True or False, then followed by the items in exc_time
+        # corresponding to the true or false, eg: [[True, [mon_1st_shift_start, mon_1st_shift_end]], [False, [mon_2nd_shift_start, mon_2nd_shift_end]]...]
+        # the above is the case if the first shift in monday is selected and the second shift is not. And it will produce these results for all days in the week,
+        # again this list will have a length of 21
+
         btn_exc_time = list(map(list, zip(bool_list_btns, exc_time)))
+
+        # this loop goes from 0 to 20 as an index, and looks to see if the value associated with btn_exc_time[i][0], or the boolean value, is True. If it is true,
+        # self.exclude_time list appends the value associated with btn_exc_time[i][1], which a list of dates and times corresponding to the start of the shift that
+        # was selected and the end of that same shift. This list will be of varying length depending on how many shifts need to be excluded
 
         for i in range(0, len(btn_exc_time)):
             if btn_exc_time[i][0]:
                 self.exclude_time.append(btn_exc_time[i][1])
 
     def clear(self):
+        # this function is called when the clear button on the GUI is pressed. It should only be pressed if dates and times were selected to be excluded but a mistake
+        # was made. In that case the exclude_time list can be cleared and the dates can be re selected
         self.exclude_time.clear()
 
     def getData(self):
+        # this function is called when the Get Data button on the GUI is pressed. It retrieves the csv file, reads it, parses the data, returns a graph of the trip
+        # times and saves this graph as well as a text document that includes the information about the trips
+
+
         sns.set(style="whitegrid")
         tips = sns.load_dataset("tips")
+
+        # the code below is reading the csv from the self.url_csv_retrieval link that we created in the weekSelection function, it then transforms it into a
+        # pandas dataframe. With this datafram we set the column names and drop the columns we do not want, lettered A, B ... We then change the Time column
+        # to a time object and subtract the 6 hours we had to add earlier to the csv link for timezone. We then trim the current column to get a readable number
+        # and then convert it to a numeric object. Set the index to Time and resample to 10 seconds to change from ~600,000 data points to ~60,00, reset index back
+        # to normal. Create a Current_S column to compare Current to its next value. Trip column is created based on the Current value being above 10 mA, and
+        # Current_S value being below 1. This will be a True value. Create a Recover column where Current_S is greater than Current, ie: filling SR, and Current_S
+        # is greater than 220 mA, ie: stored.
 
         df = pd.read_csv(self.url_csv_retrieval)
         df.columns = ['Time', 'Current', 'A', 'B', 'C']
@@ -412,15 +448,28 @@ class Ui_Weekly_Stats_Application(object):
 
         df = df.set_index(['Time'])
 
-        for i in range(0, len(exclude_time)):
-            s = pd.to_datetime(exclude_time[i][0])
-            e = pd.to_datetime(exclude_time[i][1])
+        # With the index set as Time we can parse out some information that we want to exclude, ie: exclusion times. This loop goes over the length of the
+        # self.exclude_time list and sets the start value = self.exclude_time[i][0], first value, and end time to [i][1], second value. We can then use these
+        # values in the df.loc method to remove any times that are within each range of the exclude_time list.
+
+        for i in range(0, len(self.exclude_time)):
+            s = pd.to_datetime(self.exclude_time[i][0])
+            e = pd.to_datetime(self.exclude_time[i][1])
             df = df.loc[(df.index < s) | (df.index > e)]
+
+        # reset index to normal and create a column Time_shift based on Time next value. Create a new column Time_Subtract that subtracts Time from Tme_shift.
+        # we then convert to a readable values of hours only.
 
         df.reset_index(inplace=True)
         df['Time_shift'] = df['Time'].shift(-1)
         df['Time_Subtract'] = ((df['Time_shift'] - df['Time']).dt.seconds / 3600 +
                                (df['Time_shift'] - df['Time']).dt.days * 24)
+
+        # Using the Time_Subtract column we loop over the entire df looking for values where Time_Subtract is greater than 7 hours, ie: Maint/Dev shift
+        # If the current at that location is less than 1, we want to set the Recovery equal to True, That way at location i we are entering an exclusion shift,
+        # if trip has yet to be recovered, we want to indicated that this is a recovery so we can have an accurate amount of downtime. If the Current at the
+        # i location plus 1, ie coming out of a shift is less than 1 then we want to set the Trip equal to true so we can again account for downtime that isn't
+        # necessarily a trip
 
         for i in range(0, len(df)):
             if df['Time_Subtract'][i] > 7:
@@ -429,17 +478,29 @@ class Ui_Weekly_Stats_Application(object):
                 if df['Current'][i + 1] < 1:
                     df.loc[i + 1, 'Trip'] = True
 
+        # These two if statements look at the beginning and end of the week. Since we are always starting at Sunday 00:00:00, we may have started with a trip from
+        # the previous week and thats why we need to set the Trip equal to True, to account for downtime. Likewise, if the Current in the last row is still less than
+        # 1 we need to set the Recovery equal to True so we can have an accurate account of downtime even if we haven't recovered
+
         if df['Current'][0] < 1:
             df.loc[0, 'Trip'] = True
         last_row = len(df) - 1
         if df['Current'][last_row] < 1:
             df.loc[last_row, 'Recover'] = True
 
+        # This loop will create two lists one for the start time of the recovery and one for the end time of the recovery(kind of). The start time for the trips
+        # is going to be correct due to the conditions we have. However, the recovery takes all accounts where current is increasing and above 220 so we will have
+        # a lot of values in this list.
+
         for i in range(0, len(df)):
             if df['Trip'][i] == True:
                 rec_s.append(pd.to_datetime(df['Time'][i]))
             if df['Recover'][i] == True:
                 rec_e.append(pd.to_datetime(df['Time'][i]))
+
+        # This loop fixes the above problem, it first loops over rec_s list, for the value in rec_s it loops over rec_e, if the value in rec_e is greater than the
+        # value in rec_s, it appends this value to a new list rec. It the breaks the inner for loop and goes to the next value in rec_s. This will allow us to have
+        # the same number of items in each list and we will have lists that have a start and end time
 
         for i in rec_s:
             for v in rec_e:
@@ -449,14 +510,13 @@ class Ui_Weekly_Stats_Application(object):
 
         trip_recovery_times = []
         num_of_trips = []
+
+        # trip_list is a list of lists that has a start and end time from the lists rec_s and rec
+
         trip_list = list(map(list, zip(rec_s, rec)))
 
-        # Script to save output file
-        path = "/home/richart/AOD/Weekly_Stats_Docs/"  # Linux Path
-        # path = r"H:\Documents\Projects\Weekly_Stats_Docs/"  # Windows Path
-        filename = "{}_{}-Document.txt".format(self.sun, self.sat)
-        file = path + filename
-        sys.stdout = open(file, "w")
+        # This loop calculates each trip time we have and assigns a number to each trip, 1,2,3,... it appends these values to trip_recovery_times and num_of_trips
+        # respectively
 
         for i in range(0, len(trip_list)):
             print('Trip #:', i + 1, 'start time', trip_list[i][0])
@@ -500,11 +560,18 @@ class Ui_Weekly_Stats_Application(object):
         plt.xticks(y_pos, num_of_trips, )
 
         # Saving the plot as an image
-        path = "/home/richart/AOD/Weekly_Stats_Docs/"  # Linux Path
-        # path = r'H:\Documents\Projects\Weekly_Stats_Docs/'  # Windows Path
+        # path = "/home/richart/AOD/Weekly_Stats_Docs/"  # Linux Path
+        path = r'H:\Documents\Projects\Weekly_Stats_Docs/'  # Windows Path
         filename = "{}_{}-Graph.png".format(self.sun, self.sat)
         file = path + filename
         fig.savefig(file, bbox_inches=None, dpi=None)
+
+        # Script to save output file
+        # path = "/home/richart/AOD/Weekly_Stats_Docs/"  # Linux Path
+        path = r"H:\Documents\Projects\Weekly_Stats_Docs/"  # Windows Path
+        filename = "{}_{}-Document.txt".format(self.sun, self.sat)
+        file = path + filename
+        sys.stdout = open(file, "w")
 
         # Show graphic
         plt.show()
