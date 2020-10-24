0a25b6cdeb6e13be68177b8d336329d9e9e3594f
Travis-Richard <t.richard008@gmail.com>
2020-10-24 05:07:39 -0600

    added changes to add exclusions to a master file Exclusion_Shifts_Master.csv

--- Schedule_Request.py
+++ Schedule_Request.py
@@ -1,18 +1,37 @@
 import requests
 import pandas as pd
+import os
 
-response = requests.get("https://user.lightsource.ca/api/v1/schedule/30/modes/").json()
-df = pd.json_normalize(response)
-df = df.drop(columns=['id', 'tags', 'cancelled', 'rendering', 'display', 'kind', 'description', 'tentative'])
+path = r'C:\Users\richart\Documents\Projects\Weekly_Stats_Documents/'
+filename = 'Exclusion_Shifts_Master.csv'
+file = path + filename
+
+# using cycle 30, 31 and 32 in link below to create list
+
+response = requests.get("https://user.lightsource.ca/api/v1/schedule/32/modes/").json()
+exclusions = pd.json_normalize(response)
+exclusions = exclusions.drop(columns=['id', 'tags', 'cancelled', 'rendering', 'display', 'kind', 'description', 'tentative'])
 
 
-df = df[df.name != 'N']
-df = df[df.name != 'NS']
+exclusions = exclusions[exclusions.name != 'N']
+exclusions = exclusions[exclusions.name != 'NS']
 
-df['start'] = pd.to_datetime(df['start']) - pd.Timedelta(hours = 6)
-df['start'] = df['start'].dt.tz_localize(None)
+exclusions['start'] = pd.to_datetime(exclusions['start']) - pd.Timedelta(hours = 6)
+exclusions['start'] = exclusions['start'].dt.tz_localize(None)
 
-df['end'] = pd.to_datetime(df['end']) - pd.Timedelta(hours = 6,seconds = 1)
-df['end'] = df['end'].dt.tz_localize(None)
+exclusions['end'] = pd.to_datetime(exclusions['end']) - pd.Timedelta(hours = 6,seconds = 1)
+exclusions['end'] = exclusions['end'].dt.tz_localize(None)
 
-df = df.sort_values(by = 'start')
+exclusions = exclusions.sort_values(by = 'start')
+
+
+
+if os.path.isfile(file):
+    exclusions.to_csv(file, mode='a', header=False, index=False)
+    exclusions = pd.read_csv(file)
+    exclusions['start'] = pd.to_datetime(exclusions['start'])
+    exclusions = exclusions.sort_values(by='start')
+    exclusions.to_csv(file, index=False)
+
+else:
+    exclusions.to_csv(file, index=False)
