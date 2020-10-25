2f4b3fc0f4807132d8bbc31e0f324e08c3674f09
Travis-Richard <t.richard008@gmail.com>
2020-10-24 23:30:45 -0600

    Divided any exclusion longer than 8 hours into separate 8 hour shifts to allow for proper searching when looking at each week individually

--- Schedule_Request.py
+++ Schedule_Request.py
@@ -19,20 +19,54 @@ exclusions = exclusions[exclusions.name != 'NS']
 exclusions['start'] = pd.to_datetime(exclusions['start']) - pd.Timedelta(hours = 6)
 exclusions['start'] = exclusions['start'].dt.tz_localize(None)
 
-exclusions['end'] = pd.to_datetime(exclusions['end']) - pd.Timedelta(hours = 6,seconds = 1)
+# exclusions['end'] = pd.to_datetime(exclusions['end']) - pd.Timedelta(hours = 6,seconds = 1)
+exclusions['end'] = pd.to_datetime(exclusions['end']) - pd.Timedelta(hours = 6)
 exclusions['end'] = exclusions['end'].dt.tz_localize(None)
 
 exclusions = exclusions.sort_values(by = 'start')
+exclusions.reset_index(inplace=True)
+exclusions=exclusions.drop(columns=['index', 'name'])
+
+
+
+multi_shift_exc_start = []
+multi_shift_exc_end = []
+
+
+
+for i in range(len(exclusions)):
+    if exclusions['end'][i] - pd.Timedelta(hours=8) != exclusions['start'][i]:
+        exc_range = pd.date_range(start=exclusions['start'][i], end=exclusions['end'][i], freq='8H')
+        exclusions.drop(i, inplace=True)
+
+        for i in range(len(exc_range[:-1])):
+            multi_shift_exc_start.append(exc_range[i])
+            multi_shift_exc_end.append(exc_range[i] + pd.Timedelta(hours=8))
+
+start = exclusions['start'].tolist()
+end = exclusions['end'].tolist()
+
+for i in multi_shift_exc_start:
+    start.append(i)
+
+for i in multi_shift_exc_end:
+    end.append(i)
+
+exclusions = pd.DataFrame(list(zip(start, end)), columns=['Start', 'End'])
+exclusions = exclusions.sort_values(by = 'Start')
+exclusions['End'] = exclusions['End'] - pd.Timedelta(seconds=1)
 
 
 
 if os.path.isfile(file):
     exclusions.to_csv(file, mode='a', header=None, index=False)
-    # exclusions = pd.read_csv(file)
-    # exclusions.columns = ['start', 'end', 'shift']
-    # exclusions['start'] = pd.to_datetime(exclusions['start'])
-    # exclusions = exclusions.sort_values(by='start')
-    # exclusions.to_csv(file, header=None, index=False)
-
+#
+#
+#     # exclusions = pd.read_csv(file)
+#     # exclusions.columns = ['start', 'end', 'shift']
+#     # exclusions['start'] = pd.to_datetime(exclusions['start'])
+#     # exclusions = exclusions.sort_values(by='start')
+#     # exclusions.to_csv(file, header=None, index=False)
+#
 else:
     exclusions.to_csv(file, header=None, index=False)
